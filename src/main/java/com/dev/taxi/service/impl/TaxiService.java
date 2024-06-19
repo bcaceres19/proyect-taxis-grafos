b0cc2b7a8bbb762e377161d48be3e78886e8e81a
bcaceres <bacg20044@gmail.com>
2024-06-19 16:55:05 -0500

    Se agregar linea de bug

--- src/main/java/com/dev/taxi/service/impl/TaxiService.java
+++ src/main/java/com/dev/taxi/service/impl/TaxiService.java
@@ -81,6 +81,7 @@ public class TaxiService implements ITaxiService {
                 { 0,  0,  0,  0, 18,  9,  2,  0 }  // Zona T
         };
         copiris = new int[aristas.length][aristas[0].length];
+        volverLlenar();
     }
 
     /**
